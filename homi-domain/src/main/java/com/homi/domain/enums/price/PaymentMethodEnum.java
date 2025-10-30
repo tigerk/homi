7972746c501e7bb7ddb820f53241abcfd353dec0
金无邪 <hwawoon@163.com>
2025-10-30 16:10:27 +0800

    Update PriceConfigDialog and related constants
    Add new constants for payment and price methods, update the PriceConfigDialog component to use these constants, and refactor the other fees section into a table format. Additionally, adjust the default values and options for payment and price methods in various files.

---
+++ homi-domain/src/main/java/com/homi/domain/enums/price/PaymentMethodEnum.java
@@ -0,0 +1,26 @@
+package com.homi.domain.enums.price;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PaymentMethodEnum {
+    /**
+     * 房间状态枚举
+     */
+    RENT(0, "随房租付"),
+    ALL(1, "一次性全支付"),
+    MONTH(2, "月付"),
+    BI_MONTH(3, "2月付"),
+    QUARTER(4, "季付"),
+    HALF_YEAR(5, "半年付"),
+    YEAR(6, "年付"),
+    ;
+
+    private final Integer code;
+
+    private final String name;
+
+
+}
