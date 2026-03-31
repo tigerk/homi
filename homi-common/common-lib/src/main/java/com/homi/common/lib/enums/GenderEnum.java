26b7b74aa70f731c49eec3bb2ee8703b71cd71b4
tigerk <kimhwawoon@gmail.com>
2026-03-31 23:25:17 +0800

    Add `name` field to `GenderEnum` and use Lombok annotations for getter and constructor
    This commit adds a `name` field to the `GenderEnum` enum and uses Lombok's `@Getter` and `@AllArgsConstructor` annotations to generate the getter method and constructor.

--- homi-common/common-lib/src/main/java/com/homi/common/lib/enums/GenderEnum.java
+++ homi-common/common-lib/src/main/java/com/homi/common/lib/enums/GenderEnum.java
@@ -1,35 +1,34 @@
 package com.homi.common.lib.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * 性别枚举
  * <p>
  * {@code @author} tk
  * {@code @date} 2025/4/17 01:22
  */
+@Getter
+@AllArgsConstructor
 public enum GenderEnum {
 
     /**
      * 未知
      */
-    UNKNOWN(0),
+    UNKNOWN(0, "未知"),
 
     /**
      * 男
      */
-    MALE(1),
+    MALE(1, "男"),
 
     /**
      * 女
      */
-    FEMALE(2);
+    FEMALE(2, "女");
 
-    private final int value;
+    private final int code;
 
-    GenderEnum(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    private final String name;
 }
