b4601615826c4dbee03275a1d87d748f75cb6e8b
tigerk <kimhwawoon@gmail.com>
2026-05-02 00:05:37 +0800

    refactor(database): 移除业主合同相关冗余表和字段
    - 删除 owner_contract_doc 表及相关索引和数据迁移语句
    - 删除 owner_contract 表中冗余的合同模板、合同内容、签署状态及介质字段
    - 移除 file_attach 表中 biz_subtype 字段及其索引
    - 取消对 file_attach 表中合同文件业务子类型的更新逻辑
    - 清理与业主合同签约文档相关的所有历史 SQL 脚本

--- docs/sql/2026-04-30_owner_contract_doc_no.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `owner_contract_doc`
-  CHANGE COLUMN `contract_no` `doc_no` varchar(64) NOT NULL COMMENT '签约合同文档编号';
-
-UPDATE `owner_contract_doc` d
-JOIN `owner_contract` c ON c.`id` = d.`owner_contract_id`
-SET d.`doc_no` = CONCAT(c.`contract_no`, '-DOC-01')
-WHERE d.`doc_no` = c.`contract_no`;
